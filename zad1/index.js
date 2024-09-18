bc03ce2aa0302d966e7934ba177c535ad052c69e
jaruzalito <marcel.jaruzal@gmail.com>
2024-09-18 15:42:09 +0200

    drugie zajecia

--- zad1/index.js
+++ zad1/index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
-
+const url = require('url');
 
 const server = http.createServer((req, res) => {
     if (req.url === '/') {
@@ -41,6 +41,30 @@ const server = http.createServer((req, res) => {
                 res.end(data);
             }
         });
+    }
+    else if (url.parse(req.url).pathname === '/get_params') {
+        const urlparse = url.parse(req.url, true);
+        console.log(urlparse.query);
+
+        const paramsArray =Object.entries(urlparse.query);
+
+        const timestamp = Date.now();
+
+        const fileName = `params_${timestamp}.json`;
+        fs.writeFile(fileName, JSON.stringify(paramsArray), err => {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log(`Parameters written to file: ${fileName}`);
+            }
+        });
+
+        // Return a JSON response with {'ok': 'ok'}
+        res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+        const jsonResponse = {
+            ok: 'ok'
+        };
+        res.end(JSON.stringify(jsonResponse));
     }
     else {
         res.writeHead(404, { 'Content-Type': 'text/plain; charset=utf-8' });
